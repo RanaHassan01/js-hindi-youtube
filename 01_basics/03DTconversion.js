a4c4725e75c5e730ab89ed8c0e205dbeda8cc7bd
RanaHassan01 <01ranahassan@gmail.com>
2024-07-30 09:10:48 +0000

    conversionofDatatypes

---
+++ 01_basics/03DTconversion.js
@@ -0,0 +1,32 @@
+let score = 33
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber= Number(score);
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" =>33
+//"33abc"=>NAN(not a number)
+//null=>1
+//undefined=>NAN
+//"Rana hassan"=> NAN
+//true=>1
+//flase=>0
+
+
+let IsLoggedIn= "7"
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+//1=>true
+//0=>false
+//""=>false
+//"Rana"=>true
+//5,6,7etc=>true
+
+let someNumber=33
+let stringsomeNumber= String (someNumber)
+console.log(stringsomeNumber)
+console.log(typeof stringsomeNumber)
