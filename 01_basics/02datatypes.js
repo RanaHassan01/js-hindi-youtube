f21fa99ea0d575bef946317a14832acff546471c
RanaHassan01 <01ranahassan@gmail.com>
2024-07-30 08:38:32 +0000

    config done

---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version//
+//alert(3+3)//we are using nodejs not browser
+console.log(3+3) ; 
+console.log("RANA")
+let name =  "Rana Hassan"
+let age = 21
+let isLoggedIn = false 
+
+
+//number => 2 to the power of 53
+//bigint
+//string=> ""
+//boolean=>true/false
+//null=>stand alone value and treates as objects
+//undefined =>
+//symbol => unique
+
+
+
+//object 
+console.log(typeof "Rana")
+console.log(typeof age)
+console.log(typeof null) // it is object
+console.log(typeof undefined) // it is undefined
+
+
