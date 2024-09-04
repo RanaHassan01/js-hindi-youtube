9f30a72c896316b38a66114322f64aa0ea56f234
RanaHassan01 <01ranahassan@gmail.com>
2024-09-04 14:47:21 +0000

    done

---
+++ 01_basics/06num_maths.js
@@ -0,0 +1,13 @@
+const score=400;
+const balance= new Number (200);
+console.log(balance);
+console.log(score);
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+const othernum= 123.9876;
+console.log(othernum.toPrecision(4))
+
+const hundred =1000000;
+console.log(hundred.toLocaleString);
+
